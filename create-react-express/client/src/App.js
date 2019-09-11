34e1e8ed6bcbc697bc6c8a3e5c01a4da55ae12b4
conniepope <conniepope77@gmail.com>
2019-09-11 13:21:08 -0400

    set up component files & structure, model, mongoose server

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Saved from "./components/Saved";
 import "./App.css";
 
 class App extends Component {
@@ -7,12 +9,19 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <Header>
+
+          </Header>
+          <Search>
+
+          </Search>
+          <Saved></Saved>
+          {/* <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2> */}
         </div>
-        <p className="App-intro">
+        {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
