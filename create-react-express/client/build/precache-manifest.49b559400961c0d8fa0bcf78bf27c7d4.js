10c2b06b560bad0d8b4ca09e67b15a335d1bdd27
ConniePope <47279070+conniepope@users.noreply.github.com>
2020-01-05 15:55:40 -0500

    updates to mongoose

---
+++ create-react-express/client/build/precache-manifest.49b559400961c0d8fa0bcf78bf27c7d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0721c07f6cdf6bdcee3",
+    "url": "/static/css/main.547dba85.chunk.css"
+  },
+  {
+    "revision": "d0721c07f6cdf6bdcee3",
+    "url": "/static/js/main.c0e6a071.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b9cac519de863e6d71b7",
+    "url": "/static/js/2.7b633f07.chunk.js"
+  },
+  {
+    "revision": "c0e672886e6102d0d7a51b72e58be86e",
+    "url": "/index.html"
+  }
+];
